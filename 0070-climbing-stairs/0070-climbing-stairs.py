3bb7e1768fd3e93a5e82872b5b2cc011c96c9830
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-05-03 23:22:27 -0400

    Time: 25 ms (9.46%), Space: 13.2 MB (88.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,14 +4,13 @@ class Solution(object):
             return 1
         if (n == 2):
             return 2
-        a = []
-        a.append(0)
-        a.append(1)
-        a.append(2)
+        first = 1
+        second = 2
         for i in range(3, n+1):
-            result = a[i-1]+a[i-2]
-            a.append(result)
-        return a[n] 
+            result = first + second
+            first = second
+            second = result
+        return result
         """
         :type n: int
         :rtype: int
