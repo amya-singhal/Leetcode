e1185e2700fd66146530304aae9cd27fb9af1bb4
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-05-03 22:49:14 -0400

    Time: 19 ms (46.78%), Space: 13.3 MB (61.11%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numTrees(self, n):
+        if (n == 0 or n == 1):
+            return 1
+        array = []
+        array.append(1)
+        array.append(1)
+        for j in range(2, n+1):
+            count = 0
+            for i in range(1, j+1):
+                count += array[i-1]* array[j-i]
+            array.append(count)
+        return array[n]
+            
+        """
+        :type n: int
+        :rtype: int
+        """
+        
