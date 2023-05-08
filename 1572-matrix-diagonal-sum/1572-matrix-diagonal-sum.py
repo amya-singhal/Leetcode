71712f97894c011d9ae1c72958165228cffe4a2a
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-05-08 18:51:23 -0400

    Time: 119 ms (17.39%), Space: 16.5 MB (12.27%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -4,21 +4,12 @@ class Solution:
         d1 = 0
         d2 = len(mat[0]) - 1
         x = 0
-        while (d1 != d2 and d2 != -1):
-            ans += mat[x][d1]
-            ans += mat[x][d2]
-            d1 += 1
-            d2 -= 1
-            x += 1
-        if (d2 == -1):
-            return ans
-        ans += mat[x][d1]
-        d1 += 1
-        d2 -= 1
-        x += 1
         while (d2 != -1):
-            ans += mat[x][d1]
-            ans += mat[x][d2]
+            if (d1 == d2):
+                ans += mat[x][d1]
+            else:
+                ans += mat[x][d1]
+                ans += mat[x][d2]
             d1 += 1
             d2 -= 1
             x += 1
