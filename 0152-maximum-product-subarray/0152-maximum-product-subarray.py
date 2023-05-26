abda7aa673fc11fab993ddbf3d78727ff2d22b48
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-05-25 23:28:21 -0400

    Time: 106 ms (11.03%), Space: 16.9 MB (26.76%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,10 +3,9 @@ class Solution:
         maxi = nums[0]
         localMax, localMin = maxi, maxi
         for i in range(1, len(nums)):
-            if (nums[i] < 0):
-                localMax, localMin = localMin, localMax
-            localMax = max(nums[i], nums[i]*localMax)
-            localMin = min(nums[i], nums[i]*localMin)
+            tmp = localMax*nums[i]
+            localMax = max(nums[i], tmp, localMin*nums[i])
+            localMin = min(nums[i], nums[i]*localMin, tmp)
             maxi = max(localMax, maxi)
         return maxi
         
