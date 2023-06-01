c026fd12e8b4cc7b57d961adb3200e01fc200918
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-05-31 20:01:54 -0400

    Time: 95 ms (97.02%), Space: 17.6 MB (6.22%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,19 +1,45 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         # using sliding window and hash values
-        pL = len(p)
-        sL = len(s)
-        S, P = 0, 0
-        if (sL < pL):
-            return []
+        """
         ans = []
-        for i in range(pL):
+        s -> cbaebabacd
+        p -> abc
+        lP = 10
+        lS = 3
+        S = hash(a) + hash(b) + hash(c)
+        P = hash(c) + hash(b) + hash(a) = hash(abc)
+        base condition:
+        S == P -> 
+        ans = [0]
+        for loop:
+        S = hash(b) + hash(a) + hash(e)
+        S != P
+        S = hash(a) + hash(e) + hash(b)
+        S != P
+        S = hash(e) + hash(b) + hash(a)
+        S != P
+        S = hash(b) + hash(a) + hash(b)
+        S != P
+        S = hash(a) + hash(b) + hash(a)
+        S != P
+        S = hash(a) + hash(b) + hash(c)
+        S == P
+        ans = [6]
+        """
+        ans = []
+        lP = len(p)
+        lS = len(s)
+        S,P = 0,0
+        if (lS < lP):
+            return []
+        for i in range(lP):
             S += hash(s[i])
             P += hash(p[i])
-        if (S == P):
+        if S==P:
             ans.append(0)
-        for i in range(pL, sL):
-            S += hash(s[i]) - hash(s[i - pL])
+        for i in range(lP, lS):
+            S += hash(s[i]) - hash(s[i-lP])
             if (S == P):
-                ans.append(i - pL + 1)
+                ans.append(i-lP + 1)
         return ans
