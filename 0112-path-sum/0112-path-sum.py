3891553f077b5dec109f9a2004e92defd34de9bd
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-06-11 19:47:15 -0400

    Time: 72 ms (7.04%), Space: 17.7 MB (6.23%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -1,32 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-        
-class Solution(object):
-    def hasPathSum(self, root, targetSum):
-        if (not root):
-            return False
-        currentSum = 0
-        def recur(root, targetSum, currentSum):
-            if (not root):
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
                 return False
-            if (not root.right and not root.left):
-                currentSum += root.val
-                if (currentSum == targetSum):
-                    return True
-                else:
-                    return False
-            while (root):
-                currentSum += root.val
-                return recur(root.left, targetSum, currentSum) or recur(root.right, targetSum, currentSum)
-        return recur(root, targetSum, currentSum)
-        
-        """
-        :type root: TreeNode
-        :type targetSum: int
-        :rtype: bool
-        """
-        
+        queue = [(root, targetSum - root.val)]
+        while(queue):
+            l = len(queue)
+            x, y = queue.pop(0)
+            if not x.right and not x.left and y == 0:
+                return True
+            if x.left:
+                queue.append((x.left, y- x.left.val))
+            if x.right:
+                queue.append((x.right, y - x.right.val))
+        return False
