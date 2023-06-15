5268b49a290ab113fff26b0b5d673009fdb843f6
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-06-14 20:57:18 -0400

    Time: 46 ms (49.96%), Space: 16.4 MB (24.23%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -7,12 +7,12 @@
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
-        def recur(root):
-            nonlocal ans
-            if not root:
-                return
-            recur(root.left)
+        stack = []
+        while(root or stack):
+            while(root):
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
             ans.append(root.val)
-            recur(root.right)
-        recur(root)
+            root = root.right
         return ans
