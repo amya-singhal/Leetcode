c8afb71fb93b47b0e28cf7eb80a5b89737fef64e
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-06-17 14:29:11 -0400

    Time: 247 ms (10.44%), Space: 17.2 MB (32.18%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -15,7 +15,7 @@ class Solution:
             p1, p2 = find(n1), find(n2)
             if p1 == p2:
                 return 0
-            if rank[p1] > rank[p2]:
+            if rank[p2] > rank[p2]:
                 par[p1] = p2
                 rank[p2] += rank[p1]
             else:
