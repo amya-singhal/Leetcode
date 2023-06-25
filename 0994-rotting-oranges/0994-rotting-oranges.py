c21c2fdcf51961e2a08eac10ae858a98e97185d7
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-06-24 21:37:08 -0400

    Time: 69 ms (49.77%), Space: 16.3 MB (58.97%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,10 +1,10 @@
 class Solution:
-    def check(self, grid: List[List[int]]) -> bool:
-        temp = [a for b in grid for a in b]
-        if 1 in temp: 
-            return True
-        return False
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        def check() -> bool:
+            temp = [a for b in grid for a in b]
+            if 1 in temp: 
+                return True
+            return False
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         q = []
         for i in range(len(grid)):
@@ -12,7 +12,7 @@ class Solution:
                 if grid[i][j] == 2:
                     q.append((i, j))
         minutes = 0
-        while(q and self.check(grid)):
+        while(q and check()):
             minutes += 1
             for i in range(len(q)):
                 x, y = q.pop(0)
@@ -22,7 +22,7 @@ class Solution:
                     if (0 <= xNew < len(grid) and 0 <= yNew < len(grid[0]) and grid[xNew][yNew] == 1):
                         grid[xNew][yNew] = 2
                         q.append((xNew, yNew))
-        if self.check(grid):
+        if check():
             return -1
         return minutes
                     
