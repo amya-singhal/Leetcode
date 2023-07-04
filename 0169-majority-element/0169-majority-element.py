577df9fbea26cf6492e1b129ff961d0f956a1b6f
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-07-04 07:41:08 -0400

    Time: 158 ms (98.46%), Space: 17.8 MB (73.87%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,22 +1,3 @@
 class Solution:
-    def counter(self, target: int, s: int, e: int, nums:List[int]) -> int:
-        count = 0
-        for i in range(s, e+1):
-            if nums[i] == target:
-                count += 1
-        return count
     def majorityElement(self, nums: List[int]) -> int:
-        def recur(s, e):
-            nonlocal nums
-            if s == e:
-                return nums[s]
-            m = (s + e)//2
-            l = recur(s, m)
-            r = recur(m+1, e)
-            if l == r:
-                return l
-            else:
-                if self.counter(l, s, e, nums) > self.counter(r, s, e, nums):
-                    return l
-                return r
-        return recur(0, len(nums) - 1)
+        return collections.Counter(nums).most_common()[0][0]
