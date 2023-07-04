a4583365c8123ec086a95d9b91e4daadd321d796
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-07-04 03:27:31 -0400

    Time: 44 ms (77.30%), Space: 16.2 MB (90.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,31 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        """
-        i : {()()  
-        x = ['[', '{', '(']
-        stack = {
-        stack = {
-        ()
-        stack = {
-        ()
-        stack is not empty
-        not valid
-        """
         stack = []
-        if len(s) == 0:
-            return True
-        if len(s) == 1:
-            return False
         for i in s:
-            if (i == '(' or i == '{' or i == '['):
+            if i == '(' or i == '[' or i == '{':
                 stack.append(i)
             else:
-                if (len(stack) == 0):
-                    return False # there is no open bracket preceding the closed one
-                x = stack.pop()
-                if (x == '(' and i != ')' or x == '[' and i != ']' or x == '{' and i != '}'):
+                if len(stack) == 0:
                     return False
-        if (len(stack) != 0):
+                x = stack.pop()
+                if (i == ')' and x != '(') or (i == ']' and x != '[') or (i == '}' and x != '{'):
+                    return False
+        if len(stack):
             return False
         return True
-        
