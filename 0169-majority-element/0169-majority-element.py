ce0a77cbd3c0b7ff9ef4aef316db5a134d6abe29
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-07-04 07:54:07 -0400

    Time: 174 ms (66.15%), Space: 17.9 MB (32.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,3 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return collections.Counter(nums).most_common()[0][0]
+        count = 1
+        number = nums[0]
+        for n in nums[1:]:
+            if n != number:
+                if count == 0:
+                    number = n
+                    count += 1
+                else:
+                    count -= 1
+            else:
+                count += 1
+        return number
