49678a85045d3c88e4ac146814c5bbb070f1166b
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-08-18 00:56:43 -0400

    Time: 41 ms (94.28%), Space: 16.4 MB (92.91%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -3,17 +3,16 @@ class Solution:
         l1 = len(s1)
         l2 = len(s2)
         l3 = len(s3)
-        if l1+l2 != l3:
+        if l3 != l2 + l1:
             return False
-        dp = [[False] * (l2 + 1) for i in range(l1 + 1)]
-        dp[0][0]= True
+        dp = [[False]* (l2+1) for i in range(l1+1)]
+        dp[0][0] = True
         for i in range(1, l1+1):
             dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
-        for j in range(1, l2+1):
-            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+        for i in range(1, l2+1):
+            dp[0][i] = dp[0][i-1] and s2[i-1] == s3[i-1]
         for i in range(1, l1+1):
             for j in range(1, l2+1):
-                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
-        # print(dp)
+                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i-1+j]) or (dp[i][j-1] and s2[j-1] == s3[i-1+j])
         return dp[l1][l2]
-            
+        
