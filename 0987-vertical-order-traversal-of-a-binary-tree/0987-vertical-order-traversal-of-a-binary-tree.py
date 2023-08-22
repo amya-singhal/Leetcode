a55d29ed486ea50ed0f18c03ae58f6f18a9434e2
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-08-22 00:04:19 -0400

    Time: 39 ms (87.16%), Space: 16.7 MB (55.72%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,32 +6,23 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        array = [] # index, level, value of node
+        # array = [] # index, level, value of node
+        array = defaultdict(list)
         def bfs(root, level, index):
             if not root:
                 return
-            array.append([index, level, root.val])
+            array[index].append([root.val, level])
             bfs(root.left, level+1, index-1)
             bfs(root.right, level+1, index+1)
         bfs(root, 0, 0)
-        array.sort()
-        # [[-1,1,9] [0,0,3] [0,2,15]...]
-        q = array[0][0]
-        tmp = []
         verticalTraversal = []
-        for i, l, node in array:
-            if i == q:
-                tmp.append(node)
-            else:
-                verticalTraversal.append(tmp)
-                q = i
-                tmp = [node]
-        if len(tmp) != 0:
+        for i in sorted(array.keys()):
+            l = array[i]
+            l.sort(key=lambda x:(x[1], x[0]))
+            tmp = []
+            for element in l:
+                tmp.append(element[0])
             verticalTraversal.append(tmp)
-        # tmp = [9]
-        # tmp = [3], [3, 15]
-        # tmp = [20]
-        # tmp = [7]
         return verticalTraversal
 
                 
