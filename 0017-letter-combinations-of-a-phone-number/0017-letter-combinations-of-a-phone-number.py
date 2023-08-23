69c8ac4499a325a32020b67ec32eacd99f4d7d68
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-08-23 18:57:01 -0400

    Time: 41 ms (53.12%), Space: 16.3 MB (51.79%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,24 +1,19 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        """
-        23
-        2 -> a,b,c
-        3 -> d,e,f
-        bf: n^2
-        
-        
-        """
-        ans = []
-        d = {'2' : 'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno','7':'pqrs','8': 'tuv', '9':'wxyz'}
-        if (len(digits) == 0):
+        # tmp = ""
+        # ans = []
+        # helper function- base case, len(tmp) == len digits
+        # dict = {}
+        d = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7': 'pqrs', '8':'tuv', '9':'wxyz'}
+        if len(digits) == 0:
             return []
-        def helper(tmp, digits):
-            if (len(digits) == 0):
-                ans.append(tmp)
+        strCombination = []
+        def helper(digits, tmp):
+            if len(digits) == 0:
+                strCombination.append(tmp)
+                tmp = ""
                 return
-            else:
-                for x in d[digits[0]]:
-                    helper(tmp +x, digits[1:])
-        
-        helper("", digits)
-        return ans
+            for s in d[digits[0]]:
+                helper(digits[1:], tmp+s)
+        helper(digits, "")
+        return strCombination
