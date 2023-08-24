926f8ef33684d254d96ff5faaee16f90281dbdc2
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-08-24 03:29:08 -0400

    Time: 32 ms (96.06%), Space: 16.4 MB (32.38%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,20 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        cache = {}
-        def helper(s):
-            if len(s) == 0:
-                return 1
-            if s in cache:
-                return cache[s]
-            if s[0] == '0':
-                return 0
-            if s[0] == '1' and len(s) > 1:
-                cache[s] = helper(s[1:]) + helper(s[2:])
-            elif s[0] == '2' and len(s) > 1 and s[1] != '7' and s[1] != '8' and s[1] != '9':
-                    cache[s] = helper(s[1:]) + helper(s[2:])
-            else:
-                cache[s] = helper(s[1:])
-            # print(cache)
-            return cache[s]
-
-        return helper(s)
+        if not s or s[0] == '0':
+            return 0
+        n = len(s)
+        dp = [0]*(n+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            if 0 < int(s[i-1:i]):
+                dp[i] = dp[i-1]
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+        return dp[-1]
