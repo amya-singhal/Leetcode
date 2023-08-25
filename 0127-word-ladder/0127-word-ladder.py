89aa33fc1fc307e0fad59499cf05a248bfcb2b42
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-08-25 02:03:21 -0400

    Time: 1754 ms (11.74%), Space: 20.3 MB (34.32%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -3,32 +3,26 @@ class Solution:
         if endWord not in wordList:
             return 0
         wordList.append(beginWord)
-        l = len(beginWord)
-        patterns = {}
+        patterns = defaultdict(list)
+        n = len(wordList)
         for word in wordList:
             for i in range(len(word)):
-                pattern = word[:i] + "*" + word[i+1:]
-                if (pattern not in patterns):
-                    patterns[pattern] = [word]
-                else:
-                    patterns[pattern].append(word)
+                tmp = word[:i]+"*"+word[i+1:]
+                patterns[tmp].append(word)
+        # print(patterns)
         queue = [(beginWord, 1)]
         visited = [beginWord]
-        if beginWord in wordList:
-            ans = 1
-        else:
-            ans = 0
-        while(queue):
+        while queue:
             for w in range(len(queue)):
-                x, y = queue.pop(0)
-                if (x == endWord):
+                x,y = queue.pop(0)
+                if x == endWord:
                     return y
                 for i in range(len(x)):
-                    pattern = x[:i] + "*" + x[i+1:]
-                    for p in patterns[pattern]:
-                        if p not in visited:
-                            queue.append((p, y+1))
-                            visited.append(p)
+                    pattern = x[:i]+"*"+x[i+1:]
+                    for word in patterns[pattern]:
+                        if word not in visited:
+                            queue.append((word, y+1))
+                            visited.append(word)   
         return 0
                 
         
