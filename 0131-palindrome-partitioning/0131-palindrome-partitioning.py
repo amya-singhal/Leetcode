71ecd4728c014cfa64fcc661f31ca383988eb09a
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-08-25 23:50:42 -0400

    Time: 570 ms (63.22%), Space: 33.7 MB (50.08%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,12 +1,27 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def dfs(s, ans, tmp):
-            if not s:
-                ans.append(tmp)
-                return
-            for i in range(1, len(s)+1):
-                if s[:i] == s[:i][::-1]:
-                    dfs(s[i:], ans, tmp+[s[:i]])
         ans = []
-        dfs(s, ans, [])
+        def checkPalindrome(string):
+            l = 0
+            r = len(string) - 1
+            while l<=r:
+                if string[l] != string[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        # visited = set()
+        def helper(s, tmp):
+            nonlocal ans
+            if len(s) == 0:
+                # print(tmp)
+                ans.append(tmp.copy())
+            for i in range(len(s)):
+                if checkPalindrome(s[:i+1]):
+                    # tmp.append(s[:i+1])
+                    helper(s[i+1:], tmp+[s[:i+1]])
+                    # tmp.remove(s[:i+1])
+        helper(s, [])
         return ans
+                
+                    
