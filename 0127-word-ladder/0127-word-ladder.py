916d0772d49757548e90fb9ef064f97fd6934846
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-11-30 23:40:57 -0500

    Time: 1746 ms (14.88%), Space: 20.5 MB (27.02%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,27 +2,30 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        wordList.append(beginWord)
-        patterns = defaultdict(list)
-        n = len(wordList)
+        if beginWord not in wordList:
+            wordList.append(beginWord)
+        wordDic = defaultdict(list)
         for word in wordList:
             for i in range(len(word)):
-                tmp = word[:i]+"*"+word[i+1:]
-                patterns[tmp].append(word)
-        # print(patterns)
-        queue = [(beginWord, 1)]
+                # pattern
+                p = word[:i] + "*" + word[i+1:]
+                wordDic[p].append(word)
         visited = [beginWord]
-        while queue:
+        queue = [(beginWord, 1)]
+        count = float(inf)
+        while(queue):
             for w in range(len(queue)):
-                x,y = queue.pop(0)
-                if x == endWord:
-                    return y
-                for i in range(len(x)):
-                    pattern = x[:i]+"*"+x[i+1:]
-                    for word in patterns[pattern]:
-                        if word not in visited:
-                            queue.append((word, y+1))
-                            visited.append(word)   
-        return 0
-                
+                (word, index) = queue.pop(0)
+                if word == endWord:
+                    count = min(count, index)
+                for i in range(len(word)):
+                    p = word[:i] + "*" + word[i+1:]
+                    for nextWord in wordDic[p]:
+                        if nextWord not in visited:
+                            queue.append((nextWord, index+1))
+                            visited.append(nextWord)
+        if count == float(inf):
+            return 0
+        else:
+            return count
         
