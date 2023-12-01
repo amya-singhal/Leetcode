9a66de37265b84fbed43d8846995663c14b13686
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2023-11-30 23:42:00 -0500

    Time: 399 ms (38.13%), Space: 17.9 MB (80.11%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,31 +1,18 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
-        if beginWord not in wordList:
-            wordList.append(beginWord)
-        wordDic = defaultdict(list)
-        for word in wordList:
-            for i in range(len(word)):
-                # pattern
-                p = word[:i] + "*" + word[i+1:]
-                wordDic[p].append(word)
-        visited = [beginWord]
-        queue = [(beginWord, 1)]
-        count = float(inf)
-        while(queue):
-            for w in range(len(queue)):
-                (word, index) = queue.pop(0)
-                if word == endWord:
-                    count = min(count, index)
-                for i in range(len(word)):
-                    p = word[:i] + "*" + word[i+1:]
-                    for nextWord in wordDic[p]:
-                        if nextWord not in visited:
-                            queue.append((nextWord, index+1))
-                            visited.append(nextWord)
-        if count == float(inf):
-            return 0
-        else:
-            return count
+        wordList = set(wordList)
         
+        queue = [(beginWord,1)]
+        
+        while queue:
+            curr, count = queue.pop(0)
+            if curr == endWord:
+                return count
+            for i in range(len(curr)):
+                for c in ascii_lowercase:
+                    x = curr[:i] + c + curr[i+1:]
+                    if x in wordList:
+                        queue.append((x, count+1))
+                        wordList.remove(x)
+        
+        return 0
