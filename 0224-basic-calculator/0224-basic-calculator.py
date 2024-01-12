3185939be139962d0b08aa8348898ee6028f6a2a
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-11 23:55:05 -0500

    Time: 62 ms (86.96%), Space: 19 MB (28.43%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,9 +1,15 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        sign = 1
-        num = 0
-        answer = 0
+        # "(1+(4+5+2)-3)+(6+8)", "-()"
+        # stack = [0,+,1,+ 
+        # answer = 0, 1, 0, 4+5 = 9+1*2 = 11+ 1 = 12
+        # sign = 1, -1
+        # curr = 0, 1, 0, 4, 0, 5, 0, 2, 0
+        # '(', output and sign in stack
+        # digit, num = digit
+        # sign'+-', add sign*num into answer and change sign 
+        # ')', add sign*num into answer, pop stack twice(1,2) and answer 1= 2
+        stack, answer, sign, num = [], 0, 1, 0
         for i in s:
             if i.isdigit():
                 num = num*10 + int(i)
@@ -17,11 +23,15 @@ class Solution:
             elif i == '(':
                 stack.append(answer)
                 stack.append(sign)
-                sign = 1
                 answer = 0
+                sign = 1
             elif i == ')':
                 answer += sign*num
-                answer *= stack.pop()
-                answer += stack.pop()
+                newsign = stack.pop()
+                prevoutput = stack.pop()
+                answer *= newsign
+                answer += prevoutput
                 num = 0
+                sign = 1
         return answer + sign*num
+                
