0c1724219b7287f40a3e2349a3e1fc7fcd5c1014
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-16 21:52:05 -0500

    Time: 148 ms (19.63%), Space: 21.4 MB (36.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,22 +1,20 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # i < j, if sj <= ei: overlap happens
-        # else, add element
-        # [1,8],[2,6],[7,10]
-        # [1,8], [7,10]
-        intervals.sort()
         if len(intervals) == 1:
             return intervals
-        merged = [intervals[0]]
+        # sorting using start time
+        intervals.sort()
+        print(intervals)
+        merge_intervals = [intervals[0]] #[[0,0]]
         for i in range(1, len(intervals)):
-            if intervals[i][0] <= merged[-1][1]:
-                merged[-1][0] = min(merged[-1][0], intervals[i][0])
-                merged[-1][1] = max(merged[-1][1], intervals[i][1])
+            start, end = merge_intervals[-1] #0,0
+            next_start = intervals[i][0] #1
+            print(start, end, next_start)
+            if next_start <= end:
+                merge_intervals.pop()
+                merge_intervals.append([min(start, intervals[i][0]), max(intervals[i][1], end)])
             else:
-                merged.append(intervals[i])
-        return merged
-    
-    # merged --> [[1,6],[8,10],[15,18]]
-    # merged --> [[1,5]]
-    # [1,8],[2,6],[7,10]
-    # merged --> [[1,10]]
+                merge_intervals.append(intervals[i])
+        return merge_intervals
+            
+        
