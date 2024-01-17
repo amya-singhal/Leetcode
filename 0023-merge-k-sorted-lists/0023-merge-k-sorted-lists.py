d3ab47d4bf7742d4af97f862fc529f84a9b0cf0f
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-16 23:39:35 -0500

    Time: 91 ms (61.97%), Space: 20.3 MB (49.08%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,30 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
+        if lists is None:
             return None
-        h  = []
+        dummy = ListNode(0)
+        curr = dummy
+        heap = []
         for i in range(len(lists)):
-            if lists[i] == None:
+            if lists[i] is None:
                 continue
-            heappush(h, (lists[i].val, i))
+            heapq.heappush(heap, [lists[i].val, i])
             lists[i] = lists[i].next
-        if not h:
-            return None
-        ansList = ListNode()
-        dummy = ansList
-        while(h):
-            # print(dummy)
-            x, y = heappop(h)
-            if lists[y]:
-                heappush(h, (lists[y].val, y))
-                lists[y] = lists[y].next
-            ansList.val = x
-            if h:
-                ansList.next = ListNode()
-                ansList = ansList.next
-        return dummy
-        
-            
-            
+        print(heap) # [[1,0],[1,1],[2,2]]
+        while (heap):
+            element, index = heapq.heappop(heap) #1,0
+            if lists[index] != None:
+                heapq.heappush(heap, [lists[index].val, index])
+                lists[index] = lists[index].next
+            curr.next = ListNode(element)
+            curr = curr.next
+        return dummy.next
             
