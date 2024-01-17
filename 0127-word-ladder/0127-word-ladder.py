c8d8db452ab0a087e4153ddb86e1fa0f79a735d9
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-17 12:59:01 -0500

    Time: 1631 ms (17.00%), Space: 20.7 MB (31.74%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,18 +1,30 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordList = set(wordList)
-        
-        queue = [(beginWord,1)]
-        
-        while queue:
-            curr, count = queue.pop(0)
-            if curr == endWord:
-                return count
-            for i in range(len(curr)):
-                for c in ascii_lowercase:
-                    x = curr[:i] + c + curr[i+1:]
-                    if x in wordList:
-                        queue.append((x, count+1))
-                        wordList.remove(x)
-        
+        if endWord not in wordList:
+            return 0
+        if beginWord not in wordList:
+            wordList.append(beginWord)
+        # dict : ['pattern': 'corresponding words']
+        patterndict = defaultdict(list)
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i]+'*'+word[i+1:]
+                patterndict[pattern].append(word)
+        # patterndict = ['*it':['hit'], 'h*t':['hit','hot']....]
+        que = [beginWord] # [dog, log]
+        visited = [beginWord] # [hit, hot, dot, lot, dog, log]
+        answer = 1 # 2,3,4
+        while que:
+            for _ in range(len(que)):
+                x = que.pop(0) # [hit]
+                if str(x) == endWord:
+                    return answer
+                for i in range(len(x)):
+                    pattern = x[:i]+'*'+x[i+1:] # *it, h*t
+                    values = patterndict[pattern]
+                    for y in values:
+                        if y not in visited:
+                            visited.append(y)
+                            que.append(y)
+            answer += 1
         return 0
