c2db37a252312f3dbb741abcc8a801bafdf4e8d3
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-18 19:17:41 -0500

    Time: 222 ms (14.83%), Space: 89.1 MB (35.00%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,11 +6,20 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-        else:
-            ans = TreeNode(preorder[0])
-            mid = inorder.index(preorder[0])
-            ans.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-            ans.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-            return ans
+        #  r [20,15,7] l [15,7] r [7] 
+        #  r [15,20,7] l [15] r [7]
+        # root = [3,9,None,None,20,15, None, None,7, None, None]
+        def helper(root, preorder, inorder):
+            if not preorder or not inorder:
+                return None
+            root = TreeNode(preorder[0])
+            index = 0
+            for i in range(len(inorder)):
+                if inorder[i] == root.val:
+                    index = i
+                    break
+            root.left = helper(root.left, preorder[1:i+1], inorder[:i])
+            root.right = helper(root.right, preorder[i+1:], inorder[i+1:])
+            return root
+        answer = helper(None, preorder, inorder)
+        return answer
