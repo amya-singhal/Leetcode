c01438021a0c826b854528c590bfebc05bba9127
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-21 12:14:57 -0500

    Time: 3981 ms (77.57%), Space: 16.5 MB (57.43%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -5,21 +5,20 @@ class Solution:
         """
         n = len(board)
         m = len(board[0])
-        def dfs(i,j, index, visited):
-            visited.append([i,j])
+        visited = set()
+        def dfs(i,j, index):
             if index == len(word):
                 return True
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-            for x,y in directions:
-                if 0 <= i+x < n and 0 <= j+y < m and board[i+x][j+y] == word[index] and [i+x,j+y] not in visited:
-                    if dfs(i+x, j+y, index + 1, visited.copy()):
-                        return True
-            return False
+            if i < 0 or i >= n or j < 0 or j >= m or board[i][j] != word[index] or (i,j) in visited:
+                return False
+            visited.add((i,j))
+            res = (dfs(i+1, j, index+1) or dfs(i-1,j,index+1) or dfs(i,j+1, index+1) or dfs(i,j-1,index+1))
+            visited.remove((i,j))
+            return res
                     
         for i in range(n):
             for j in range(m):
                 if board[i][j] == word[0]:
-                    print(i, j)
-                    if dfs(i, j, 1, []):
+                    if dfs(i, j, 0):
                         return True
         return False
