9f655a8b662fa3d628f594ffebd198c48a97a4b8
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-21 12:47:06 -0500

    Time: 36 ms (70.92%), Space: 16.8 MB (57.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        ans = [[1]*n] * m
-        for m1 in range(1, m):
-            for n1 in range(1, n):
-                ans[m1][n1] = ans[m1 - 1][n1] + ans[m1][n1 - 1]
-        return ans[-1][-1]
-            
-                
-                
-            
+        visited = set()
+        dp = {}
+        def dfs(i,j):
+            if i == m-1 or j == n-1:
+                return 1
+            elif (i,j) not in dp:
+                dp[(i,j)] = dfs(i+1,j) + dfs(i, j+1)
+            return dp[(i,j)]
+        return dfs(0,0)
