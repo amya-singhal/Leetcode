c9e4c2ed511213b0fc4ded26062f113693c57fbb
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-20 20:19:15 -0500

    Time: 59 ms (62.00%), Space: 16.6 MB (57.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,37 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        visited = {}
-        x = 0
-        finalCount = 0
-        count = 0
-        while(x < len(s)):
-            if (s[x] in visited):
-                x = visited[s[x]]+1
-                if (count > finalCount):
-                    finalCount = count
-                count = 0
-                visited = {}
-            else:
-                visited[s[x]] = x
-                count += 1
-                x += 1
-        finalCount = max(finalCount, count)
-        return finalCount
+        """ 
+        "abcadef"
+        {'a':0, 'b':1, 'c':2}
+        "abba"
+        {'a':0, 'b':2}
+        "pwwkew"
         """
-        s3-> pewpabc
-        a3-> 0120123
+        d = defaultdict(int)
+        tmp = 0 
         ans = 0
-        for i in range(0, len(s)):
-            tmp = ""
-            for j in range(i, len(s)):
-                if (s[j] in tmp):
-                    break
-                tmp += s[j]
-            if (len(tmp) > ans):
-                ans = len(tmp)
+        for i in range(len(s)):
+            if s[i] in d:
+                tmp = max(tmp, d[s[i]]+1)
+            ans = max(ans, i-tmp+1)
+            d[s[i]] = i
         return ans
-        """
-        
-        
                 
-            
+                
+                
+                
