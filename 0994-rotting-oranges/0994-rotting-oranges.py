426de3e98c33cbd6325309722969f852527de6dc
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-23 22:04:41 -0500

    Time: 45 ms (93.11%), Space: 16.4 MB (84.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,31 +2,30 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
+        directions = [(1,0), (-1,0), (0,1),(0,-1)]
+        minute = 0
+        q = []
         def check() -> bool:
             tmp = [a for b in grid for a in b]
             if 1 in tmp:
-                return True
-            return False
-        q = []
+                return False
+            return True
+
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
-                    q.append((i, j))
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        ans = 0
-        while(q and check()):
-            ans += 1
-            for i in range(len(q)):
-                (x, y) = q.pop(0)
-                for i, j in directions:
-                    newx = x + i
-                    newy = y + j
+                    q.append((i,j))
+        while (q and not check()):
+            for _ in range(len(q)):
+                (x,y) = q.pop(0)
+                for a, b in directions:
+                    newx = a+x
+                    newy = b+y
                     if 0 <= newx < n and 0 <= newy < m and grid[newx][newy] == 1:
                         grid[newx][newy] = 2
                         q.append((newx, newy))
-        # print(grid)
-        if check():
+            minute += 1
+        if not check():
             return -1
-        return ans
+        return minute
     
-     
