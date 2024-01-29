55a8937c0d0252e6377952e5420405485b173c79
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-01-28 20:38:32 -0500

    Time: 852 ms (5.01%), Space: 101.5 MB (11.15%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,32 +1,22 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        n = len(startTime)
-        d = defaultdict()
-        d[0] = 0
-        jobs = list(zip(startTime, endTime, profit))
-        jobs.sort()
-        @lru_cache(None)
-        def helper(i):
-            if i == n:
-                return 0
-            j = i+1
-            # l = i+1
-            # r = n-1
-            # ans = l
-            # while(l < r):
-            #     m = (l+r)//2
-            #     if jobs[m][0] == jobs[i][1]:
-            #         ans = m
-            #     elif jobs[m][0] < jobs[i][1]:
-            #         l = m + 1
-            #     else:
-            #         ans = m
-            #         r = m - 1
-                    
-            while (j < n and jobs[j][0] < jobs[i][1]):
-                j += 1
-            one = jobs[i][2] + helper(j)
-            two = helper(i+1)
-            return max(one, two)
-        return helper(0)
-            
+        jobs = sorted(zip(startTime, endTime, profit))
+        n = len(jobs)
+
+        def helper(val: int) -> int:
+            l, r = 0, n - 1
+            while l <= r:
+                m = (l + r) >> 1
+                s, e, p = jobs[m]
+                if s < val: l = m + 1
+                else: r = m - 1
+            return l
+
+        @cache
+        def dp(i: int) -> int:
+            if i >= n: return 0
+              
+            j = helper(jobs[i][1])
+            return max(dp(i + 1), dp(j) + jobs[i][2])
+
+        return dp(0)
