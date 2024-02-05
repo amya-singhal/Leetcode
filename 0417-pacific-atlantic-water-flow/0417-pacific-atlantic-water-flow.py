9107341a2db47cc5eb6168f80d51dbc420335027
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-02-04 19:09:59 -0500

    Time: 2731 ms (5.01%), Space: 18.1 MB (86.23%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,28 +2,27 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         n = len(heights)
         m = len(heights[0])
-        pacific = set()
-        atlantic = set()
-        
-        def dfs(r, c, visit, prevH):
-            if (r,c) in visit or r<0 or c<0 or r>=n or c>=m or heights[r][c] < prevH:
-                return
-            visit.add((r,c))
-            dfs(r+1, c, visit, heights[r][c])
-            dfs(r-1, c, visit, heights[r][c])
-            dfs(r, c+1, visit, heights[r][c])
-            dfs(r, c-1, visit, heights[r][c])
-        for r in range(n):
-            dfs(r, 0, pacific, heights[r][0])
-            dfs(r, m-1, atlantic, heights[r][m-1])
-        for c in range(m):
-            dfs(0, c, pacific, heights[0][c])
-            dfs(n-1, c, atlantic, heights[n-1][c])
         ans = []
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        atlantic, pacific = False, False
+        def check(i, j, visited):
+            nonlocal pacific, atlantic
+            visited.add((i, j))
+            if i == n-1 or j == m-1:
+                    atlantic = True
+            if i == 0 or j == 0:
+                    pacific = True
+            for x, y in directions:
+                newx, newy = x+i, y+j
+                if 0 <= newx < n and 0 <= newy < m and heights[newx][newy] <= heights[i][j] and (newx, newy) not in visited:
+                    check(newx, newy, visited)
+        
         for i in range(n):
             for j in range(m):
-                if (i,j) in pacific and (i,j) in atlantic:
-                    ans.append([i, j])
+                atlantic, pacific = False, False
+                check(i, j, set())
+                if pacific and atlantic:
+                    ans.append([i,j])
         return ans
                     
-            
+        
