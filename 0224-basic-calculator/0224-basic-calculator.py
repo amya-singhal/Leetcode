b29037b048a1a7f0fc599d6d9e4487cc4787c2a3
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-02-16 15:46:19 -0500

    Time: 62 ms (73.82%), Space: 18 MB (80.79%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,37 +1,38 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # "(1+(4+5+2)-3)+(6+8)", "-()"
-        # stack = [0,+,1,+ 
-        # answer = 0, 1, 0, 4+5 = 9+1*2 = 11+ 1 = 12
-        # sign = 1, -1
-        # curr = 0, 1, 0, 4, 0, 5, 0, 2, 0
-        # '(', output and sign in stack
-        # digit, num = digit
-        # sign'+-', add sign*num into answer and change sign 
-        # ')', add sign*num into answer, pop stack twice(1,2) and answer 1= 2
-        stack, answer, sign, num = [], 0, 1, 0
+        # if digits: add to stack
+        # if open brack: add to stack
+        # if close brack: 
+        # if operator: sign
+        # (1+(4+5+2)-3)+(6+8)
+        ans = 0
+        num = 0
+        z = []
+        sign = 1
         for i in s:
             if i.isdigit():
-                num = num*10 + int(i)
+                num = num*10+int(i)
+            elif i =='(':
+                z.append(ans)
+                z.append(sign)
+                ans = 0
+                sign = 1
+            elif i == ')':
+                ans += num*sign
+                newsign = z.pop()
+                output = z.pop()
+                ans = ans*newsign
+                ans = ans + output
+                num = 0
+                sign = 1   
             elif i in '+-':
-                answer += sign*num
+                ans += sign*num
                 num = 0
                 if i == '+':
                     sign = 1
-                else:
+                if i == '-':
                     sign = -1
-            elif i == '(':
-                stack.append(answer)
-                stack.append(sign)
-                answer = 0
-                sign = 1
-            elif i == ')':
-                answer += sign*num
-                newsign = stack.pop()
-                prevoutput = stack.pop()
-                answer *= newsign
-                answer += prevoutput
-                num = 0
-                sign = 1
-        return answer + sign*num
+        return ans + sign*num
+                    
                 
+        
