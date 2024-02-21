cd31b358dce8666e8bffc88d290f3dc54d5af411
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-02-21 13:49:07 -0500

    Time: 124 ms (40.10%), Space: 17.3 MB (84.54%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,33 +1,44 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
+        """
+        s = "ADOBECODEBANC", t = "ABC"
+        need = {a:1, b:1, c:1}
+        have = {a:0, d:1, o:1, b:1, e:1, c:1}
+        needc = 3
+        havec = 2
+        """
         need = defaultdict(int)
-        # {a:1,b:1,c:1}
-        for i in t:
-            need[i] += 1
         have = defaultdict(int)
-        # {a:1, d:2, o:2, b:2, e:2, c:1}
+        lT = len(t)
+        lS = len(s)
+        if lT > lS:
+            return ""
+        ans = ""
+        ansl = 0
+        ansr = lS
+        for i in range(lT):
+            need[t[i]] += 1
         needCount = len(need)
         haveCount = 0
-        # 3
-        l = 0
-        # 1
-        L, R = 0 , len(s)
-        length = float('inf')
-        # 6, 
-        for r in range(len(s)):
-            have[s[r]] += 1
-            if s[r] in need and have[s[r]] == need[s[r]]:
+        j = 0
+        for i in range(lS):
+            have[s[i]] += 1
+            if s[i] in need and have[s[i]] == need[s[i]]:
                 haveCount += 1
-            while haveCount == needCount:
-                if R-L+1 > r-l+1:
-                    L, R = l, r
-                    length = r-l+1
-                have[s[l]] -= 1
-                if s[l] in need and have[s[l]] < need[s[l]]:
+            while (needCount == haveCount):
+                if (i-j) < (ansr-ansl):
+                    ans = s[j:i+1]
+                    ansl, ansr = j, i
+                have[s[j]] -= 1
+                if s[j] in need and have[s[j]] < need[s[j]]:
                     haveCount -= 1
-                l += 1
-        if length != float('inf'):
-            return s[L:R+1]
-        return ""
+                j += 1
+        return ans
+            
+                
+        
+        
+            
+            
+            
+        
