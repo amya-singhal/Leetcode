6e5cda998611be4535194e501085b94bbd8522a8
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-02-22 18:17:04 -0500

    Time: 51 ms (71.57%), Space: 17 MB (68.33%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,28 +1,29 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         col = set()
-        pdiag = set()
-        ndiag = set()
-        board = [["."]*n for i in range(n)]
-        res = []
+        diag = set()
+        anti_diag = set()
+        tmp = []
+        ans = []
         def backtrack(r):
+            nonlocal col, diag, anti_diag, tmp, ans
             if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
-                return res
+                ans.append(tmp[:])
+                return
             for c in range(n):
-                if c in col or (r+c) in pdiag or (r-c) in ndiag:
+                if c in col or (r+c) in diag or (r-c) in anti_diag:
                     continue
                 col.add(c)
-                pdiag.add(r+c)
-                ndiag.add(r-c)
-                board[r][c] = "Q"
+                diag.add(r+c)
+                anti_diag.add(r-c)
+                tmp.append('.'*c + 'Q' + '.'*(n-c-1))
+                
                 backtrack(r+1)
+                
                 col.remove(c)
-                pdiag.remove(r+c)
-                ndiag.remove(r-c)
-                board[r][c] = "."
+                diag.remove(r+c)
+                anti_diag.remove(r-c)
+                tmp.pop()
+                
         backtrack(0)
-        return res
-        
-        
+        return ans
