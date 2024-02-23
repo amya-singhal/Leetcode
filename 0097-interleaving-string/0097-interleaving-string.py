2a3bdbc9a4a3d394e4e99c91c0235d508b9d5638
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-02-23 14:19:20 -0500

    Time: 45 ms (50.09%), Space: 17.2 MB (67.60%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -5,13 +5,17 @@ class Solution:
         ls3 = len(s3)
         if ls1 + ls2 != ls3:
             return False
-        dp = [[False] * (ls1+1) for _ in range(ls2+1)]
-        dp[0][0] = True
-        for i in range(1, ls2+1):
-            dp[i][0] = dp[i-1][0] and s2[i-1] == s3[i-1]
-        for i in range(1, ls1+1):
-            dp[0][i] = dp[0][i-1] and s1[i-1] == s3[i-1]
-        for i in range(1, ls2+1):
-            for j in range(1, ls1+1):
-                dp[i][j] = (dp[i-1][j] and s2[i-1] == s3[i-1+j]) or (dp[i][j-1] and s1[j-1] == s3[i-1+j])
-        return dp[ls2][ls1]
+        cache = {}
+        def helper(i,j,k):
+            if k == ls3:
+                return True
+            if (i,j) in cache:
+                return cache[(i,j)]
+            ans = False
+            if i < ls1 and s1[i] == s3[k]:
+                ans = ans or helper(i+1, j, k+1)
+            if j < ls2 and s2[j] == s3[k]:
+                ans = ans or helper(i, j+1, k+1)
+            cache[(i,j)] = ans
+            return ans
+        return helper(0,0,0)
