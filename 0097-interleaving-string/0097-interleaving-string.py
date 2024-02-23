706f91094bfb931e1b68d988c59f2bc93b0e2289
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-02-23 13:58:27 -0500

    Time: 44 ms (54.92%), Space: 16.6 MB (87.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,18 +1,17 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        l1 = len(s1)
-        l2 = len(s2)
-        l3 = len(s3)
-        if l3 != l2 + l1:
+        ls1 = len(s1)
+        ls2 = len(s2)
+        ls3 = len(s3)
+        if ls1 + ls2 != ls3:
             return False
-        dp = [[False]* (l2+1) for i in range(l1+1)]
+        dp = [[False] * (ls1+1) for _ in range(ls2+1)]
         dp[0][0] = True
-        for i in range(1, l1+1):
-            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
-        for i in range(1, l2+1):
-            dp[0][i] = dp[0][i-1] and s2[i-1] == s3[i-1]
-        for i in range(1, l1+1):
-            for j in range(1, l2+1):
-                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i-1+j]) or (dp[i][j-1] and s2[j-1] == s3[i-1+j])
-        return dp[l1][l2]
-        
+        for i in range(1, ls2+1):
+            dp[i][0] = dp[i-1][0] and s2[i-1] == s3[i-1]
+        for i in range(1, ls1+1):
+            dp[0][i] = dp[0][i-1] and s1[i-1] == s3[i-1]
+        for i in range(1, ls2+1):
+            for j in range(1, ls1+1):
+                dp[i][j] = (dp[i-1][j] and s2[i-1] == s3[i-1+j]) or (dp[i][j-1] and s1[j-1] == s3[i-1+j])
+        return dp[ls2][ls1]
