1ea455d6da1847efafbdba6ccda620960d796e8f
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-02-25 18:20:27 -0500

    Time: 41 ms (30.41%), Space: 16.5 MB (66.33%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,15 +7,17 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         levels = defaultdict(list)
-        def helper(root, level):
-            nonlocal levels
-            if not root:
-                return
-            levels[level].append(root.val)
-            helper(root.left, level+1)
-            helper(root.right, level+1)
-        helper(root, 0)
+        queue = [(root, 0)]
         ans = []
+        while queue:
+            l = len(queue)
+            for _ in range(l):
+                node, level = queue.pop(0)
+                if not node:
+                    continue
+                levels[level].append(node.val)
+                queue.append((node.left, level+1))
+                queue.append((node.right, level+1))
         for i in range(len(levels)):
             ans.append(levels[i][-1])
         return ans
