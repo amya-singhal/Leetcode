960f15af5e08be55afb312699031d75ebf1a1505
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-02-28 13:07:03 -0500

    Time: 318 ms (56.38%), Space: 17.5 MB (87.05%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,27 +1,21 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        ans = 1
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        m, n = len(matrix), len(matrix[0])
-        longestPath = [[0] * n for _ in range(m)]
-        
-        def longestIncreasingPath_start(i, j) -> int:
-            nonlocal matrix, directions, m, n, longestPath
-            res = 1
-            for direction in directions:
-                pos = list(map(add, [i, j], direction))
-                new_i, new_j = pos[0], pos[1]
-                
-                if new_i >= 0 and new_i < m and new_j >=0 and new_j < n and matrix[new_i][new_j] > matrix [i][j]:
-                    if longestPath[new_i][new_j] == 0:
-                        longestPath[new_i][new_j] = longestIncreasingPath_start(new_i, new_j)
-                        
-                    res = max(res, 1 + longestPath[new_i][new_j])
-                longestPath[i][j] = res
-            return res
-        
-        
-        for i in range(m):
-            for j in range(n):
-                ans = max(ans, longestIncreasingPath_start(i, j))
+        n = len(matrix)
+        m = len(matrix[0])
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        longestPath = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        ans = 0
+        def helper(i, j):
+            val = matrix[i][j]
+            maxi = 0
+            for x,y in directions:
+                newi, newj = i+x, j+y
+                if 0 <= newi < n and 0 <= newj < m and matrix[newi][newj] > val:
+                    if longestPath[newi][newj] == 0:
+                        longestPath[newi][newj] = helper(newi, newj)
+                    maxi = max(longestPath[newi][newj], maxi)
+            return maxi+1
+        for i in range(n):
+            for j in range(m):
+                ans = max(ans, helper(i, j))
         return ans
