17fe0d91663d54e1a2c37081289b458de4ba4828
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-03-03 14:47:29 -0500

    Time: 5006 ms (9.83%), Space: 17.7 MB (63.08%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,16 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
-        lastIndex = len(nums)-1
-        curr = 0
-        jumps = 0
-        while(curr < len(nums)):
-            maxReached = nums[curr]+curr
-            if maxReached >= len(nums)-1:
-                return jumps+1
-            for i in range(curr+1, maxReached+1):
-                if (nums[i]+i > nums[curr]+curr):
-                    curr = i
-            jumps += 1
-        return jumps
+        """
+        nums = [2,3,1,1,4]
+        dp = [2,1,2,1,0]
+        """
+        dp = [float('inf') for _ in range(len(nums))]
+        dp[len(nums)-1] = 0
+        for i in range(len(nums)-2, -1, -1):
+            for j in range(nums[i]):
+                if i+j+1 < len(nums):
+                    dp[i] = min(dp[i], 1+dp[i+j+1])
+        return dp[0]
