4189a3070215ea035ac07adda93a1931c5b1bd94
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-03-03 15:01:08 -0500

    Time: 2295 ms (24.00%), Space: 18.7 MB (10.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,11 +3,22 @@ class Solution:
         """
         nums = [2,3,1,1,4]
         dp = [2,1,2,1,0]
+        4
+        jump = 0
+        q = [0]
+        q = [1,2]
+        q = [3,4]
         """
-        dp = [float('inf') for _ in range(len(nums))]
-        dp[len(nums)-1] = 0
-        for i in range(len(nums)-2, -1, -1):
-            for j in range(nums[i]):
-                if i+j+1 < len(nums):
-                    dp[i] = min(dp[i], 1+dp[i+j+1])
-        return dp[0]
+        jump = 0
+        q = [0]
+        visit = set()
+        while q: # [0], [1,2], [3,4], 
+            for _ in range(len(q)):
+                x = q.pop(0) #0, 1, 2, 3, 4
+                if x == len(nums)-1:
+                    return jump
+                for i in range(1,nums[x]+1): # 1,2
+                    if i+x < len(nums) and (i+x) not in visit:
+                        q.append(i+x)
+                        visit.add(i+x)
+            jump += 1
