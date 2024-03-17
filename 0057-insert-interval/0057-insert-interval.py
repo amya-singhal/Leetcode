964b361c79cf974b8fa8c51686b14e137bcc5736
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-03-17 18:29:59 -0400

    Time: 89 ms (5.98%), Space: 19.8 MB (79.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,15 +1,26 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        """
+        intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
+        newInterval = [4,8]
         ans = []
-        for i in range(len(intervals)):
-            if (newInterval[1] < intervals[i][0]):
-                ans.append(newInterval)
-                return ans + intervals[i:]
-            elif (intervals[i][1] < newInterval[0]):
-                ans.append(intervals[i])
+        """
+        n = len(intervals)
+        if n == 0:
+            return [newInterval]
+        used = 0
+        mergedInt = []
+        for x,y in intervals:
+            if y < newInterval[0] or x > newInterval[1]:
+                mergedInt.append([x,y])
             else:
-                newInterval[0] = min(intervals[i][0], newInterval[0])
-                newInterval[1] = max (intervals[i][1], newInterval[1])
-        ans.append(newInterval)
-        return ans
-        
+                if used == 0:
+                    mergedInt.append([min(x, newInterval[0]), max(y, newInterval[1])])
+                    used = 1
+                else:
+                    i, j = mergedInt.pop()
+                    mergedInt.append([min(i, x), max(j, y)])
+        if used == 0:
+            mergedInt.append(newInterval)
+            mergedInt = sorted(mergedInt)
+        return mergedInt
