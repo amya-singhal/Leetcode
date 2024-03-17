213f7476447e8da3f8cfce5a577b6d45e5948e73
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-03-17 17:53:32 -0400

    Time: 63 ms (21.56%), Space: 16.7 MB (18.99%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
-        visit = []
-        def helper(tmp, n):
+        def helper(tmp, n, index):
             nonlocal target
             if n == target:
-                tmp = sorted(tmp)
-                if tmp not in visit:
-                    ans.append(tmp)
-                    visit.append(tmp)
-            if n > target:
+                ans.append(tmp[:])
                 return
-            for i in candidates:
-                if i+n <= target:
-                    helper(tmp+[i], i+n)
-        helper([], 0)
+            if n > target or index >= len(candidates):
+                return
+            tmp.append(candidates[index])
+            helper(tmp, n+candidates[index], index)
+            tmp.pop()
+            helper(tmp, n, index+1)
+        helper([], 0, 0)
         return ans
                     
