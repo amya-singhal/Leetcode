b870a5842a010ab5d6359fb978de2a6f24e2d4b7
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-03-28 18:48:37 -0400

    Time: 42 ms (29.10%), Space: 17 MB (6.56%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,24 +1,24 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        """"
-        n = 3
-        tmp -> n*2 -> add it to the ans
-        l and r
-        l < n:
-        add lP
-        and whenever r < l:
-         we can add rP
         """
-        l = 0
-        r = 0
-        ansArray = []
-        def dfs(l, r, tmp):
-            if (len(tmp) == n*2):
-                ansArray.append(tmp)
+        n = 3
+        ["((()))","(()())","(())()","()(())","()()()"]
+        
+        l,r =  2,1   2,1    3,0
+        tmp= ["()(", "(()", "((("]
+        l = r = n
+        """
+        ans = []
+        
+        def helper(l, r, tmp):
+            if r == n:
+                ans.append(tmp)
                 return
-            if (l < n):
-                dfs(l+1, r, tmp+'(')
-            if (r < l):
-                dfs(l, r+1, tmp+')')
-        dfs(0,0,"")
-        return ansArray
+            if l < n:
+                helper(l+1,r,tmp+'(')
+            if r < l:
+                helper(l,r+1,tmp+')')
+        helper(0,0,"")
+        return ans
+            
+        
