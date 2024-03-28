767427922fe1704c52322d11e7670763c7a05be7
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-03-28 18:41:34 -0400

    Time: 37 ms (45.91%), Space: 16.5 MB (89.37%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -3,8 +3,6 @@ class Solution:
         phone = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
         ans = []
         n = len(digits)
-        if n == 0:
-            return ans
         def helper(index, tmp):
             if index >= n:
                 ans.append(tmp)
@@ -12,7 +10,8 @@ class Solution:
             s = phone[digits[index]]
             for i in s:
                 helper(index+1, tmp+i)
-        helper(0, "")
+        if digits:
+            helper(0, "")
         return ans
             
         
