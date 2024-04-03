06d00e38e87ed76f55728f91a54c55cbd9d77478
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-04-02 23:50:15 -0400

    Time: 38 ms (75.97%), Space: 16.8 MB (36.72%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        l = len(nums)
-        def recur(tmp):
-            if (len(tmp) == l):
-                ans.append(tmp.copy())
+        # []
+        n = len(nums)
+        def helper(index, tmp):
+            if index >= n:
+                ans.append(tmp)
                 return
             for i in nums:
-                if (i not in tmp):
-                    tmp.append(i)
-                    recur(tmp)
-                    tmp.pop()
-            
-        recur([])
+                if i not in tmp:
+                    helper(index+1, tmp+[i])
+        helper(0, [])
         return ans
+            
