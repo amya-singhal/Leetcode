0100862fab4ac9cb779303ca5914ae0e73757c61
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-04-03 00:02:20 -0400

    Time: 801 ms (5.06%), Space: 16.7 MB (96.25%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def helper(index, tmp, visit):
-            if index >= len(nums):
+        def helper(tmp, visit):
+            if len(tmp) == len(nums):
                 if tmp not in ans:
                     ans.append(tmp)
             for i in range(len(nums)):
                 if i not in visit:
                     visit.add(i)
-                    helper(index+1, tmp+[nums[i]], visit)
+                    helper(tmp+[nums[i]], visit)
                     visit.remove(i)
-        helper(0, [], set())
+        helper([], set())
         return ans
