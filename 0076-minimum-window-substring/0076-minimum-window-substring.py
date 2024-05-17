3b308c0524944420dc97b2f09d1e465e2d4f2929
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-05-17 10:15:09 -0400

    Time: 176 ms (29.84%), Space: 17.4 MB (10.46%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,44 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        """
-        s = "ADOBECODEBANC", t = "ABC"
-        need = {a:1, b:1, c:1}
-        have = {a:0, d:1, o:1, b:1, e:1, c:1}
-        needc = 3
-        havec = 2
-        """
-        need = defaultdict(int)
+        need = Counter(t)
         have = defaultdict(int)
-        lT = len(t)
-        lS = len(s)
-        if lT > lS:
-            return ""
-        ans = ""
-        ansl = 0
-        ansr = lS
-        for i in range(lT):
-            need[t[i]] += 1
         needCount = len(need)
         haveCount = 0
-        j = 0
-        for i in range(lS):
-            have[s[i]] += 1
-            if s[i] in need and have[s[i]] == need[s[i]]:
-                haveCount += 1
-            while (needCount == haveCount):
-                if (i-j) < (ansr-ansl):
-                    ans = s[j:i+1]
-                    ansl, ansr = j, i
-                have[s[j]] -= 1
-                if s[j] in need and have[s[j]] < need[s[j]]:
+        ansL = float(inf)
+        ans = ""
+        l,r = 0,0
+        while (r < len(s)):
+            while needCount == haveCount:
+                if ansL > r-l+1:
+                    ansL = r-l+1
+                    ans = s[l:r]
+                have[s[l]] -= 1
+                if s[l] in need and have[s[l]] < need[s[l]]:
                     haveCount -= 1
-                j += 1
+                l += 1
+            else:
+                have[s[r]] += 1
+                if s[r] in need and have[s[r]] == need[s[r]]:
+                    haveCount += 1
+                r += 1
+        while needCount == haveCount:
+                if ansL > r-l+1:
+                    ansL = r-l+1
+                    ans = s[l:r]
+                have[s[l]] -= 1
+                if s[l] in need and have[s[l]] < need[s[l]]:
+                    haveCount -= 1
+                l += 1
         return ans
-            
-                
-        
-        
-            
-            
-            
-        
