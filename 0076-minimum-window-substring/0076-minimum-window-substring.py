60d73d1ca6dac735ed531d3c64dc38afc357c96a
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-05-17 10:19:21 -0400

    Time: 172 ms (31.26%), Space: 17.6 MB (5.56%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -7,21 +7,12 @@ class Solution:
         ansL = float(inf)
         ans = ""
         l,r = 0,0
-        while (r < len(s)):
+        for i in range(len(s)):
+            have[s[r]] += 1
+            if s[r] in need and have[s[r]] == need[s[r]]:
+                haveCount += 1
+            r += 1
             while needCount == haveCount:
-                if ansL > r-l+1:
-                    ansL = r-l+1
-                    ans = s[l:r]
-                have[s[l]] -= 1
-                if s[l] in need and have[s[l]] < need[s[l]]:
-                    haveCount -= 1
-                l += 1
-            else:
-                have[s[r]] += 1
-                if s[r] in need and have[s[r]] == need[s[r]]:
-                    haveCount += 1
-                r += 1
-        while needCount == haveCount:
                 if ansL > r-l+1:
                     ansL = r-l+1
                     ans = s[l:r]
