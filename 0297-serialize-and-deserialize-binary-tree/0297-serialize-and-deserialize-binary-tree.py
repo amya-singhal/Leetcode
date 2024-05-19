e5b3c90ffe91c34cb5b4962c9ba8cdcbd6a4c8e9
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-05-18 23:49:00 -0400

    Time: 95 ms (29.78%), Space: 20.4 MB (86.02%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -6,43 +6,50 @@
 #         self.right = None
 
 class Codec:
-    # None --> '_'
+
     def serialize(self, root):
-        s = ""
-        def helper(root):
-            nonlocal s
-            if not root:
-                s += '_'
-                return
-            s += str(root.val)
-            s += ","
-            helper(root.left)
-            s += ","
-            helper(root.right)
-        helper(root)
-        return s
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-      
+        ans = ""
+        def helper(root):
+            nonlocal ans
+            if not root:
+                ans += '_,'
+            else:
+                ans += str(root.val)
+                ans += ','
+                helper(root.left)
+                helper(root.right)
+        helper(root)
+        return ans[:len(ans)-1]
+        
+
     def deserialize(self, data):
-        que = deque(data.split(","))
-        def helper(que):
-            element = que.popleft()
-            if element == '_':
-                return None
-            answer = TreeNode(int(element))
-            answer.left = helper(que)
-            answer.right = helper(que)
-            return answer
-        return helper(que)
         """Decodes your encoded data to tree.
         
         :type data: str
         :rtype: TreeNode
         """
+        if len(data) == 0:
+            return None
+        q = deque(data.split(","))
+        def helper():
+            nonlocal q
+            if not q:
+                return None
+            x = q.popleft()
+            if x == '_':
+                ans = None
+            else:
+                ans = TreeNode(int(x))
+                ans.left = helper()
+                ans.right = helper()
+            return ans
+        return helper()
+            
         
 
 # Your Codec object will be instantiated and called as such:
