78a1a430a6cf3beb494984c726f982f21e8c2fb6
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-05-29 18:29:33 -0400

    Time: 46 ms (37.87%), Space: 16.8 MB (91.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,24 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        """
-        [2,4,5,6,7,0,1], 0
-        [4,5,6,7,0,1,2]
-        [5,6,7,0,1,2,4]
-        """
-        l = 0
-        r = len(nums)-1
-        while l <= r:
-            m = l + (r-l) // 2
+        # binary search for the pivot
+        l, r = 0, len(nums)-1
+        while (l <= r):
+            m = l+(r-l)//2
+            # print(l,r,m)
             if nums[m] == target:
                 return m
-            if nums[m] >= nums[l]:
-                if nums[l] <= target <= nums[m]:
+            if nums[l] <= nums[m]:
+                if nums[m] >= target >= nums[l]:
                     r = m-1
                 else:
                     l = m+1
             else:
-                if nums[m] <= target <= nums[r]:
+                if nums[r] >= target >= nums[m]:
                     l = m+1
                 else:
                     r = m-1
-        return -1
+        return -1 
