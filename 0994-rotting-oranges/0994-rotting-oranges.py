90955af085f8c1e9f8f28eaa5095fda08c774135
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-05-29 19:05:35 -0400

    Time: 53 ms (38.91%), Space: 16.6 MB (61.41%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,31 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        directions = [(1,0), (-1,0), (0,1),(0,-1)]
-        minute = 0
-        q = []
-        def check() -> bool:
-            tmp = [a for b in grid for a in b]
-            if 1 in tmp:
-                return False
-            return True
-
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 2:
-                    q.append((i,j))
-        while (q and not check()):
-            for _ in range(len(q)):
-                (x,y) = q.pop(0)
-                for a, b in directions:
-                    newx = a+x
-                    newy = b+y
-                    if 0 <= newx < n and 0 <= newy < m and grid[newx][newy] == 1:
+        d = [(0,1),(0,-1),(1,0),(-1,0)]
+        m = len(grid)
+        n = len(grid[0])
+        visited = []
+        count = 0
+        q = [] 
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2 and (i,j) not in visited:
+                    q.append((i,j, 0))
+                    
+        while q:
+            l = len(q)
+            for _ in range(l):
+                x,y, level = q.pop(0)
+                visited.append((i,j))
+                for i,j in d:
+                    newx, newy = x+i, y+j
+                    if 0 <= newx < m and 0 <= newy < n and grid[newx][newy] == 1:
+                        count = max(count, level+1)
                         grid[newx][newy] = 2
-                        q.append((newx, newy))
-            minute += 1
-        if not check():
-            return -1
-        return minute
-    
+                        q.append((newx, newy, level+1))
+            
+                        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    return -1
+        return count
