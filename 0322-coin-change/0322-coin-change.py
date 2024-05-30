5729ea0516df179785fc13901cb95d96cc88de46
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-05-30 19:44:09 -0400

    Time: 867 ms (38.68%), Space: 16.9 MB (88.77%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,22 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        """
-        coins = [1,2,5], amount = 11
-        q = [(0,0)]
-        q = [(1,1),(2,1),(5,1)]
-        q = [(5,1),(3,2),(6,2),(4,2),(7,2)]
-        """
-        q = deque()
-        q.append((0,0))
-        visit = set()
-        while q:
-            cur_coin, count = q.popleft()
-            if cur_coin == amount:
-                return count
-            
+        dp = [float('inf') for _ in range(amount+1)]
+        dp[0] = 0
+        if amount == 0:
+            return 0
+        for i in range(amount+1):
             for coin in coins:
-                next_coin = cur_coin+coin
-                if next_coin <= amount and next_coin not in visit:
-                    visit.add(next_coin)
-                    q.append((next_coin, count+1))
-        return -1
+                if i == coin:
+                    dp[i] = 1
+                elif coin < i:
+                    dp[i] = min(dp[i], dp[i-coin]+1)
+        if dp[amount] == float('inf'):
+            return -1
+        return dp[amount]
