6e0551547ee425381e50cc3f31ee189b2546384e
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-07-03 16:47:58 -0400

    Time: 50 ms (26.25%), Space: 17.1 MB (17.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,37 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        a = len(matrix)
-        b = len(matrix[0])
-        if a == 1 and b == 1:
-            if matrix[0][0] != target:
-                return False
-            else:
-                return True
-        # print(a, b)
-        l = 0
-        r = len(matrix)-1
-        while(l<r):
-            m = (l+r)//2
-            if matrix[m][b-1] == target:
-                return True
-            if matrix[m][0] <= target and matrix[m][b-1] > target:
-                l = m
+        n,m = len(matrix), len(matrix[0])
+        l,r = 0, n-1
+        reqRow = -1
+        while (l <= r):
+            mid = (l+r) // 2
+            if matrix[mid][0] <= target <= matrix[mid][m-1]:
+                reqRow = mid
                 break
-            elif matrix[m][b-1] > target:
-                r = m - 1
+            if l == mid and r == mid:
+                return False
+            elif target > matrix[mid][m-1]:
+                l = mid + 1
+                reqRow = l
             else:
-                l = m + 1
-        l1 = 0
-        r1 = len(matrix[0])-1
-        while(l1 < r1):
-            m1 = (l1+r1)//2
-            if matrix[l][m1] == target:
+                r = mid - 1
+        l,r = 0, m-1
+        while (l <= r):
+            mid = (l+r) // 2
+            if matrix[reqRow][mid] == target:
                 return True
-            elif matrix[l][m1] > target:
-                r1 = m1 - 1
+            elif target > matrix[reqRow][mid]:
+                l = mid + 1
             else:
-                l1 = m1 + 1
-        if matrix[l][l1] == target:
-            return True
+                r = mid - 1
         return False
-            
+        
+        
