11763751ffb4cb6f36e4b32cbe388c09d110266b
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-08-01 21:18:54 -0400

    Time: 50 ms (25.07%), Space: 19.6 MB (14.82%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,14 +6,19 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        l = []
-        def dfs(root):
-            nonlocal l
-            if not root:
-                return
-            dfs(root.left)
-            l.append(root.val)
-            dfs(root.right)
-        dfs(root)
-        return l[k-1]
-            
+        h = []
+        q = [root]
+        while q:
+            l = len(q)
+            for _ in range(l):
+                x = q.pop(0)
+                heappush(h, x.val)
+                if x.left:
+                    q.append(x.left)
+                if x.right:
+                    q.append(x.right)
+        ans = -1
+        for i in range(k):
+            ans = heappop(h)
+        return ans
+        
