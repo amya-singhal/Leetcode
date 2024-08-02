c4844360fee93fb5157a617b0451b57772c0ae45
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-08-02 00:50:16 -0400

    Time: 237 ms (68.86%), Space: 19 MB (57.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,19 +5,31 @@ class Solution:
         directions = [(1,0), (0,1), (-1,0), (0,-1)]
         numberOfIslands = 0
         
-        def dfs(i, j):
-            nonlocal directions
-            grid[i][j] = '-1'
-            for x, y in directions:
-                newi, newj = i+x, j+y
-                if 0 <= newi < m and 0 <= newj < n and grid[newi][newj] == '1':
-                    dfs(newi, newj)
-            return
         
+#         def dfs(i, j):
+#             nonlocal directions
+#             grid[i][j] = '-1'
+#             for x, y in directions:
+#                 newi, newj = i+x, j+y
+#                 if 0 <= newi < m and 0 <= newj < n and grid[newi][newj] == '1':
+#                     dfs(newi, newj)
+#             return
+        
+        def iterativeDfs(p, q):
+            stack = [(p, q)]
+            while stack:
+                i, j = stack.pop()
+                grid[i][j] = '-1'
+                for x, y in directions:
+                    newi, newj = i+x, j+y
+                    if 0 <= newi < m and 0 <= newj < n and grid[newi][newj] == '1':
+                        stack.append((newi, newj))
+    
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
-                    dfs(i, j)
+                    # dfs(i, j)
+                    iterativeDfs(i, j)
                     numberOfIslands += 1
                     
         return numberOfIslands
