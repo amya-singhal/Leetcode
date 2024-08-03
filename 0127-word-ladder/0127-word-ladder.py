4604d695bb52729e72e835ea6e9dc524b2eeb17f
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-08-03 02:35:07 -0400

    Time: 1157 ms (18.60%), Space: 22.1 MB (17.85%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,30 +1,28 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
         if beginWord not in wordList:
             wordList.append(beginWord)
-        # dict : ['pattern': 'corresponding words']
-        patterndict = defaultdict(list)
+        if endWord not in wordList:
+            return 0
+        patterns = defaultdict(list)
+        
         for word in wordList:
             for i in range(len(word)):
-                pattern = word[:i]+'*'+word[i+1:]
-                patterndict[pattern].append(word)
-        # patterndict = ['*it':['hit'], 'h*t':['hit','hot']....]
-        que = [beginWord] # [dog, log]
-        visited = [beginWord] # [hit, hot, dot, lot, dog, log]
-        answer = 1 # 2,3,4
-        while que:
-            for _ in range(len(que)):
-                x = que.pop(0) # [hit]
-                if str(x) == endWord:
-                    return answer
-                for i in range(len(x)):
-                    pattern = x[:i]+'*'+x[i+1:] # *it, h*t
-                    values = patterndict[pattern]
-                    for y in values:
-                        if y not in visited:
-                            visited.append(y)
-                            que.append(y)
-            answer += 1
+                pattern = word[:i] + '*' + word[i+1:]
+                patterns[pattern].append(word)
+        
+        q = [(beginWord, 1)]
+        visited = set()
+        while q:
+            x, length = q.pop(0)
+            visited.add(x)
+            if x == endWord:
+                return length
+            for i in range(len(x)):
+                pattern = x[:i] + '*' + x[i+1:]
+                values = patterns[pattern]
+                for word in values:
+                    if word not in visited:
+                        q.append((word, length+1))
         return 0
+        
