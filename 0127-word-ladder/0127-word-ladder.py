f36ac8c8c650c9cd028d1a168d0d6ab24db1cab7
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-08-03 02:42:30 -0400

    Time: 1116 ms (18.77%), Space: 20.4 MB (24.89%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,28 +2,29 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        wordList.append(beginWord)
-        # dict : ['pattern': 'corresponding words']
-        patterndict = defaultdict(list)
+        patterns = defaultdict(list)
+        
         for word in wordList:
             for i in range(len(word)):
-                pattern = word[:i]+'*'+word[i+1:]
-                patterndict[pattern].append(word)
-        # patterndict = ['*it':['hit'], 'h*t':['hit','hot']....]
-        que = [beginWord] # [dog, log]
-        visited = set([beginWord])
-        answer = 1 # 2,3,4
-        while que:
-            for _ in range(len(que)):
-                x = que.pop(0) # [hit]
-                if str(x) == endWord:
+                pattern = word[:i] + '*' + word[i+1:]
+                patterns[pattern].append(word)
+        
+        q = [beginWord]
+        answer = 1
+        visited = set()
+        while q:
+            lenQ = len(q)
+            for i in range(lenQ):
+                x = q.pop(0)
+                visited.add(x)
+                if x == endWord:
                     return answer
                 for i in range(len(x)):
-                    pattern = x[:i]+'*'+x[i+1:] # *it, h*t
-                    values = patterndict[pattern]
-                    for y in values:
-                        if y not in visited:
-                            visited.add(y)
-                            que.append(y)
+                    pattern = x[:i] + '*' + x[i+1:]
+                    values = patterns[pattern]
+                    for word in values:
+                        if word not in visited:
+                            q.append(word)
             answer += 1
         return 0
+        
