f52954f7883e7d5de31b7f129ba3a55b9f32cf3f
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-08-28 09:12:47 -0400

    Time: 27 ms (93.87%), Space: 16.3 MB (94.08%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,24 +1,22 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        low = 0  # Minimum number of unmatched '('
-        high = 0  # Maximum number of unmatched '('
-
+        l, h = 0, 0
         for char in s:
             if char == '(':
-                low += 1
-                high += 1
+                l += 1
+                h += 1
+                
             elif char == ')':
-                if low > 0:
-                    low -= 1
-                high -= 1
-            else:  # char == '*'
-                if low > 0:
-                    low -= 1
-                high += 1
-
-            # If at any point, the number of unmatched '(' is negative, it's invalid
-            if high < 0:
+                if l > 0:
+                    l -= 1
+                h -= 1
+                
+            else:
+                if l > 0:
+                    l -= 1
+                h += 1
+        
+            if h < 0:
                 return False
-
-        # Valid if and only if all '(' can be matched
-        return low == 0
+        
+        return l == 0
