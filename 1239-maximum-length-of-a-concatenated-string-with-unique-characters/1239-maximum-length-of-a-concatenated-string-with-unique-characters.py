6117bd058e3bf68b7b59563c4bc67ddc0da83e6b
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-08-30 19:55:01 -0400

    Time: 786 ms (10.74%), Space: 16.6 MB (76.51%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,19 +1,10 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        def check_unique(s: str) -> bool:
-            unique = set()
-            for char in s:
-                if char in unique:
-                    return False
-                unique.add(char)
-            return True
-        
-        
         final_ans = 0
         def get_substrings(index, tmp):
             nonlocal final_ans
             if index == len(arr):
-                if check_unique(tmp):
+                if len(tmp) == len(set(tmp)):
                     final_ans = max(final_ans, len(tmp))
                 return
             get_substrings(index+1, tmp+arr[index])
