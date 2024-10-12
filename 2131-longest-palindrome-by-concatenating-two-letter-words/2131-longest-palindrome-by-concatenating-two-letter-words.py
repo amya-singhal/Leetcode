9d116c94e62fd10ae2ffd34981338be16889f724
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2024-10-11 20:06:19 -0400

    Time: 1053 ms (67.58%), Space: 41.4 MB (7.96%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -3,23 +3,24 @@ class Solution:
         d = defaultdict(int)
         for word in words:
             d[word] += 1
-        ans = 0
-        middle = False
+        total = 0
+        double = 0
+        hasSingle = False
         visited = set()
-        for word in words:
-            # print(d, word, ans)
+        for word in d.keys():
+            # print(word)
             if word[0] == word[1]:
-                if d[word] > 1:
-                    # print(d[word] // 2, d[word]%2)
-                    ans += 4*(d[word] // 2)
-                    d[word] = d[word]%2
-                if not middle and d[word] > 0:
-                    ans += 2
-                    d[word] -= 1
-                    middle = True
-            elif d[word[::-1]] > 0 and d[word] > 0:
-                ans += 4
-                d[word] -= 1
-                d[word[::-1]] -= 1
-        
-        return ans
+                if d[word] % 2 == 1:
+                    hasSingle = True
+                total += (d[word] // 2)
+                # print(total, word)
+            else:
+                rev = word[1]+word[0]
+                if rev in d:
+                    c_rev = d[rev]
+                    double += min(d[word], c_rev)
+        # print(total, double)
+        total = (total + double // 2)*4
+        if hasSingle:
+            total += 2
+        return total
