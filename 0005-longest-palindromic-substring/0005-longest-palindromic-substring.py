504ec62082c30daa6bbc5594817d1dfb698a3c54
amya-singhal <67829842+amya-singhal@users.noreply.github.com>
2025-03-03 14:19:58 -0500

    Time: 547 ms (39.47%), Space: 18.1 MB (29.5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,27 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # s = "babad"
-        # array = ['b', 'bab', 'aba', 'a', 'd']
-        # s = "cbbd"
-        # array = ['bb', 'c','b', 'b', 'd']
-        ans = ""
-        for i in range(len(s)):
+        longestPalin = ""
+        # odd len
+        for i in range(0, len(s)):
             l, r = i-1, i+1
-            tmp = s[i]
+            temp = s[i]
             while (l >= 0 and r < len(s) and s[l] == s[r]):
-                tmp = s[l]+tmp+s[r]
+                temp = s[l] + temp + s[r]
                 l -= 1
                 r += 1
-            if len(ans) < len(tmp):
-                ans = tmp
-        for i in range(len(s)-1):
+            if len(temp) > len(longestPalin):
+                longestPalin = temp
+        # even len
+        for i in range(0, len(s)-1):
             l, r = i, i+1
-            tmp = ""
+            temp = ""
             while (l >= 0 and r < len(s) and s[l] == s[r]):
-                tmp = s[l]+tmp+s[r]
+                temp = s[l] + temp + s[r]
                 l -= 1
                 r += 1
-            if len(ans) < len(tmp):
-                ans = tmp
-        return ans
-                
+            if len(temp) > len(longestPalin):
+                longestPalin = temp
+        return longestPalin
+        
